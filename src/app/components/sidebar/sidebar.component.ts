cf699012043565d29d4aaacfa12ae3edd0a1d209
Agustin Schilling <schillingagustin@gmail.com>
2024-08-26 21:12:22 -0300

    Agrego boton de logout

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatListModule, MatIcon],
+  imports: [MatListModule, MatIcon, MatButton],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  constructor(private router: Router) {}
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
