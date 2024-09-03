c98b4a3084eb2efbadbfbef8846402aeb5969af0
nicobastien <nicobastien927@gmail.com>
2024-09-03 14:53:58 -0300

    Navegación con la barra lateral.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
@@ -12,9 +12,15 @@ import { Router } from '@angular/router';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  constructor(private router: Router) {}
+  @Output() navigationChange = new EventEmitter<string>();
+
+  constructor(private router: Router) { }
   logout() {
     localStorage.clear();
     this.router.navigate(['/login']);
+  }
+
+  setNavigationWindow(window: string) {
+    this.navigationChange.emit(window);
   }
 }
